b85bede919de296187a86f82552a397150daf8fe
Stepan <offish.sk@icloud.com>
2024-02-10 18:49:11 +0300

    dz_2

--- src/channel.py
+++ src/channel.py
@@ -1,8 +1,8 @@
 from googleapiclient.discovery import build
-import isodate
+
 import os
 from dotenv import load_dotenv
-from pprint import pprint
+
 import json
 
 load_dotenv('../.env')
@@ -14,49 +14,107 @@ moscowpython_list = []
 
 class Channel:
     """Класс для ютуб-канала"""
-    __youtube = build('youtube', 'v3', developerKey=apy_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+
         self.__channel_id = channel_id
-        self.channel = self.__youtube.channels().list(id =self.__channel_id, part ='snippet,statistics').execute()
+        self.youtube = self.get_service()
+        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = f"https://www.youtube/channels/{self.__channel_id}"
         self.subscriber_count = int(self.channel['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(self.channel['items'][0]['statistics']['videoCount'])
+        self.view_count = int(self.channel['items'][0]['statistics']['viewCount'])
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pprint(json.dumps(self.channel, indent=2, ensure_ascii=False))
 
-    def to_json(self, filename):
-        data = json.dumps(self.channel)
-        with open(filename, 'w', encoding='utf-8') as f:
-            f.write(data)
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
 
     @property
     def channel_id(self):
         return self.__channel_id
 
-    @property
-    def title(self):
-        return self.channel['items'][0]['snippet']['title']
-
-    @property
-    def video_count(self):
-        return self.channel['items'][0]['statistics']['videoCount']
-
-    @property
-    def url(self):
-        return self.channel['items'][0]['snippet']['thumbnails']
-
     @classmethod
     def get_service(cls):
-        return cls.__youtube
+        api_key: str = os.getenv('API-KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def to_json(self, file_json):
+        json_data = {
+            'channel_id': self.__channel_id,
+            'title': self.title,
+            'descrition': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'videoCount': self.video_count,
+            'viewCount': self.view_count
+        }
+        with open(file_json, 'w', encoding="utf-8") as f:
+            f.write(json.dumps(json_data, ensure_ascii=False, indent=4))
+
+    @channel_id.setter
+    def channel_id(self, value):
+        self.__channel_id = value
 
     def __str__(self):
         return f"{self.title}, {self.url}"
 
     def __add__(self, other):
+        """Оператор сложения"""
         if isinstance(other, Channel):
             return int(self.subscriber_count) + int(other.subscriber_count)
         else:
             raise TypeError("Неподдерживаемый тип операнда для +: 'Channel' и {}".format(type(other)))
+    def __sub__(self, other):
+        """оператор вычитания"""
+        if isinstance(other, Channel):
+            return int(self.subscriber_count) - int(other.subscriber_count)
+        else:
+            raise TypeError("Неподдерживаемый тип операнда для -: 'Channel' и {}".format(type(other)))
 
+    def __lt__(self, other):
+        """оператор меньше (<)"""
+        if isinstance(other, Channel):
+            return int(self.subscriber_count) < int(other.subscriber_count)
+        else:
+            raise TypeError("Неподдерживаемый тип операнда для <: 'Channel' и {}".format(type(other)))
+
+    def __le__(self, other):
+        """оператор меньше или равно (<=)"""
+        if isinstance(other, Channel):
+            return int(self.subscriber_count) <= int(other.subscriber_count)
+        else:
+            raise TypeError("Неподдерживаемый тип операнда для <=: 'Channel' и {}".format(type(other)))
+
+    def __eq__(self, other):
+        """оператор равенства (==)"""
+        if isinstance(other, Channel):
+            return int(self.subscriber_count) == int(other.subscriber_count)
+        else:
+            raise TypeError("Неподдерживаемый тип операнда для ==: 'Канал' и {}".format(type(other)))
+
+    def __ne__(self, other):
+        """оператор неравенства (!=)"""
+        if isinstance(other, Channel):
+            return int(self.subscriber_count) != int(other.subscriber_count)
+        else:
+            raise TypeError("Неподдерживаемый тип операнда для !=: 'Channel' и {}".format(type(other)))
+
+    def __gt__(self, other):
+        """оператор больше (>)"""
+        if isinstance(other, Channel):
+            return int(self.subscriber_count) > int(other.subscriber_count)
+        else:
+            raise TypeError("Неподдерживаемый тип операнда для >: 'Channel' и {}".format(type(other)))
+
+    def __ge__(self, other):
+        """оператор больше
+        или равно (>=)"""
+        if isinstance(other, Channel):
+            return int(self.subscriber_count) >= int(other.subscriber_count)
+        else:
+            raise TypeError("Неподдерживаемый тип операнда для >=: 'Channel' и {}".format(type(other)))
